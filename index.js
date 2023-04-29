f6ee7b0f32a62e9a4d70ffcd0074a8c0a3afee45
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-04-28 21:06:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,34 @@ const pergunta = prompt("Escreva uma frase")
 console.log(pergunta.toUpperCase());
 console.log(pergunta.replaceAll("o","i"))
 console.log(pergunta.length)
+
+const numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+console.log(numeros[0]);
+
+const dogs = ["Xitsu", "Akita", "Dobbermann", "Bulldog", "Pooddle"];
+const dog = (prompt("Insira uma raça de cachorro"))
+
+dogs.splice(1,1)
+
+dogs.push(numero)
+
+dogs.pop()
+
+dogs[numero] === undefined ?
+console.log("você deve informar números entre 0 e 4") :
+
+console.log(dogs);
+console.log(dogs[numero]);
+console.log(dogs.length);
+console.log(dogs.includes("caramelo")) 
+
+exercicio 4
+
+const numeros = [1,2,3,4,5,6]
+console.log(numeros.length);
+console.log(numeros);
+numeros.push(7)
+numeros.splice(3,2)
+console.log(numeros);
+console.log(numeros.length);
